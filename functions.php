e7bd592fc615e6b68891dc9962dbb21c83b958cf
bybookishbabe-cmyk <bybookishbabe@gmail.com>
2026-05-16 10:38:51 -0400

    Port By Bookish Babe homepage styling

--- functions.php
+++ functions.php
@@ -11,11 +11,17 @@ add_action(
 	'wp_enqueue_scripts',
 	static function (): void {
 		wp_enqueue_style(
+			'wordpress-theme-fonts',
+			'https://fonts.googleapis.com/css2?family=Allura&family=Cormorant+Garamond:wght@400;500;600;700&family=Kaushan+Script&display=swap',
+			array(),
+			null
+		);
+
+		wp_enqueue_style(
 			'wordpress-theme-main',
 			get_theme_file_uri('assets/css/main.css'),
-			array(),
+			array('wordpress-theme-fonts'),
 			wp_get_theme()->get('Version')
 		);
 	}
 );
-
