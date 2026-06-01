e8eb072d469de52825213cd183d1bdc2f6e0819c
bybookishbabe-cmyk <bybookishbabe@gmail.com>
2026-06-01 08:15:54 -0700

    Use member state for homepage society CTA

--- template-parts/hero-smut-sentiment.php
+++ template-parts/hero-smut-sentiment.php
@@ -13,6 +13,11 @@ $hero_primary_label   = get_theme_mod('hero_primary_label', 'explore library');
 $hero_primary_link    = get_theme_mod('hero_primary_link', '/library/');
 $hero_secondary_label = get_theme_mod('hero_secondary_label', 'join the society');
 $hero_secondary_link  = get_theme_mod('hero_secondary_link', 'https://thesmutandsentimentsociety.substack.com/subscribe');
+
+if (function_exists('bbb_reader_is_society') && bbb_reader_is_society()) {
+	$hero_secondary_label = 'the society';
+	$hero_secondary_link  = function_exists('bbb_page_url') ? bbb_page_url('smut-sentiment-society') : home_url('/smut-sentiment-society/');
+}
 ?>
 
 <section class="hero-custom">
